a696f988524b7ffa946d236df55944a9c7a6fed9
df <d>
2013-09-23 18:17:39 +0800

    update version 1.0

---
+++ src/main/java/cn/ohyeah/gameserver/server/Main.java
@@ -0,0 +1,18 @@
+package cn.ohyeah.gameserver.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+
+import cn.ohyeah.gameserver.cfg.SpringConfig;
+
+public class Main {
+	private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+	public static void main(String[] args) {
+		LOG.debug("Starting application context");
+		AbstractApplicationContext ctx = new AnnotationConfigApplicationContext(SpringConfig.class);
+		ctx.registerShutdownHook();
+	}
+
+}
