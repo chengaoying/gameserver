b1e0dc2ead1875c6a53b6dc9e0282db1b827d151
df <d>
2013-10-12 18:17:10 +0800

    update version1.2 --201310121817

--- src/main/java/cn/ohyeah/gameserver/global/ErrorCode.java
+++ src/main/java/cn/ohyeah/gameserver/global/ErrorCode.java
@@ -3,7 +3,7 @@ package cn.ohyeah.gameserver.global;
 public class ErrorCode {
 	
 	public static final int EC_UNKNOW_ERROE = -10000;			//服务器未知错误
-	public static final int EC_CENTER_SERVER_ERROR = -10001;	//连接中央服务器错误
+	public static final int EC_CENTER_SERVER_ERROR = -10001;	//连接中央服务器失败
 	public static final int EC_PARSE_JSON_ERROR = -10002;		//解析json数据错误
 
 	public static String getErrorMsg(int errorCode){
@@ -11,7 +11,7 @@ public class ErrorCode {
 		case EC_UNKNOW_ERROE:
 			return "服务器未知错误";
 		case EC_CENTER_SERVER_ERROR:
-			return "连接中央服务器错误";
+			return "连接中央服务器失败";
 		case EC_PARSE_JSON_ERROR:
 			return "解析json数据错误";
 		default:
